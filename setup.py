3f91bba04b84062d99f8806fa976d7c09846fe5d
Youngchae James Chee <litcoderr@gmail.com>
2020-08-15 16:54:14 +0900

    master: upload 0.0.1 version to pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name = 'importify',
+    packages = ['importify'],
+    version = '0.0.1',     
+    license = 'MIT',
+    description = 'Import and export configuration easily',
+    author = 'Youngchae James Chee',
+    author_email = 'litcoderr@gmail.com',
+    url = 'https://github.com/litcoderr/importify',
+    download_url = 'https://github.com/litcoderr/importify/archive/0.0.1.tar.gz',
+    keywords = ['Utility', 'Json', 'Configuration'],
+    install_requires = [],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6' 
+    ]
+)
